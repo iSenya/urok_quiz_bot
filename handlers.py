f9fc3f0d1f4c27f7e07145df4a69964585dd2479
Kseniia Isavnina <senyaisavnina@gmail.com>
2020-11-01 21:58:43 +0200

    question last 15 days

--- handlers.py
+++ handlers.py
@@ -95,7 +95,7 @@ class DBCommands:
 
     async def get_random_question(self):
         row = await self.pool.fetchrow(
-            self.GET_RANDOM_QUESTION, datetime.utcnow() - timedelta(days=90)
+            self.GET_RANDOM_QUESTION, datetime.utcnow() - timedelta(days=15)
         )
         return row
 
